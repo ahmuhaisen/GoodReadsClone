b8c632c7dd6f21d372534600a7fefdd62c600eed
Ahmad Muahsien <ahmuhaisen03@gmail.com>
2024-07-18 10:53:30 +0300

    Correct File Structure #2

--- GoodReadersClone.Application/Features/Books/Handlers/GetBookByIdQueryHanler.cs
+++ GoodReadersClone.Application/Features/Books/Handlers/GetBookByIdQueryHanler.cs
@@ -1,5 +1,4 @@
 ﻿using GoodReadersClone.Application.DTOs.Books;
-using GoodReadersClone.Application.Features.Books.Commands;
 using GoodReadersClone.Application.Features.Books.Queries;
 
 namespace GoodReadersClone.Application.Features.Books.Handlers;
@@ -44,59 +43,6 @@ public class GetBookByISBNQueryHanler(
         {
             Success = true,
             Data = result
-        };
-    }
-}
-
-
-public class CreateBookCommandHandler(
-    IUnitOfWork _unitOfWork,
-    IMapper _mapper
-    )
-    : IRequestHandler<CreateBookCommand, ApiResponse>
-{
-    public async Task<ApiResponse> Handle(CreateBookCommand request, CancellationToken cancellationToken)
-    {
-        if (!_unitOfWork.AuthorRepository.IsExist(x => x.Id == request.Request.AuthorId))
-            return new ApiResponse { Message = "Author Not Found" };
-
-        if (await _unitOfWork.BookRepository.IsExist(x => x.Title == request.Request.Title))
-            return new ApiResponse { Message = "There exist a book with same title" };
-
-        if (request.Request.FirstPublished > DateTime.Now)
-            return new ApiResponse { Message = "Invalid FirstPublished Date" };
-
-        var bookToAdd = new Book();
-
-        bookToAdd = _mapper.Map<Book>(request.Request);
-
-        if (request.Request.Cover is not null)
-        {
-            //Save Image
-        }
-        bookToAdd.CoverURL = $"{FileManager.BooksCoversPath}\\{Guid.NewGuid().ToString()}.jpg";
-
-        if (!string.IsNullOrEmpty(request.Request.Genres))
-        {
-            var genresToAttach = request.Request.Genres.Split(',').Select(x => x.Trim());
-            foreach (var genreName in genresToAttach)
-            {
-                var genre = await _unitOfWork.GenreRepository.GetAsync(x => x.Name == genreName);
-                if (genre is not null)
-                    bookToAdd.Genres.Add(genre);
-            }
-        }
-
-        _unitOfWork.BookRepository.Create(bookToAdd);
-        var result = _unitOfWork.Save();
-
-        if (result == 0)
-            return new ApiResponse { Message = "Failed to create the book" };
-
-        return new ApiResponse
-        {
-            Success = true,
-            Data = bookToAdd.Id
         };
     }
 }
--- GoodReadersClone.Infrastructure/DataAccess/Repositories/GenreRepository.cs
+++ GoodReadersClone.Infrastructure/DataAccess/Repositories/GenreRepository.cs
@@ -3,9 +3,10 @@ using GoodReadersClone.Infrastructure.DataAccess.Abstractions;
 using GoodReadersClone.Infrastructure.DataAccess.Data;
 
 namespace GoodReadersClone.Infrastructure.DataAccess.Repositories;
-public class QuoteRepository : Repository<Quote>, IQuoteRepository
+
+public class GenreRepository : Repository<Genre>, IGenreRepository
 {
-    public QuoteRepository(ApplicationDbContext _context) : base(_context)
+    public GenreRepository(ApplicationDbContext _context) : base(_context)
     {
     }
 }
